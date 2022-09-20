53a5ca27526c71a8354edcc084debf0afc440daf
Victor Labussiere <victor.labussiere95@gmail.com>
2022-09-20 05:15:24 -0300

    update fix

--- curso-trabalhandoComAPIs/projects/examples/GET.js
+++
@@ -1,6 +0,0 @@
-const express = require('express') // importação do express
-const app = express() // encapsulamento dos métodos express
-
-app.listen('3000') // criação de um localhost via express
-
-app.route('/').get((req, res) => res.send('Hello World via get API'))
